bb99199d7e4001e73d1fc5ea96e22a255800d834
lzielinski <zielu1d@gmail.com>
2013-04-28 11:12:12 +0200

    Zadanie 2 commit inicjalizacyjny

---
+++ zad2/src/zad2/Zadanie.java
@@ -0,0 +1,21 @@
+package zad2;
+
+//import java.util.Scanner;
+
+public class Zadanie {
+
+	public static void main(String[] args) {
+		
+		//Scanner scan1 = new Scanner(System.in);
+		int a = Integer.parseInt(args[1]);
+		int b = Integer.parseInt(args[2]);
+		
+		System.out.println(args[0].substring(a, b));
+	 
+		
+		
+	
+		
+	}
+
+}
